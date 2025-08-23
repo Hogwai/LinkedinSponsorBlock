61ab4379cd42d61f4d227ea5543525fcbdd59e1b
Hogwai <lilian.wernert@gmail.com>
2025-08-23 15:08:00 +0200

    Replace match by include

--- LinkedinSponsorBlock.user.js
+++ LinkedinSponsorBlock.user.js
@@ -5,7 +5,8 @@
 // @description:en  Remove sponsored posts, suggestions, and partner content on linkedin.com
 // @description:fr  Supprime les publications sponsorisées, les suggestions et le contenu en partenariat sur linkedin.com
 // @author          Hogwai
-// @match           https://www.linkedin.com/feed/*
+// @include         *://*.linkedin.*
+// @include         *://*.linkedin.*/feed/*
 // @grant           none
 // @license         MIT
 // @downloadURL     https://update.greasyfork.org/scripts/546877/LinkedinSponsorBlock.user.js
